e55fa5778e55cc0f8f9fba2357abbf72ccb94edd
chakri68 <chakridevireddy69@gmail.com>
2023-09-26 20:34:15 +0530

    updated sheets to use env variables for auth

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -3,9 +3,22 @@ import { parseData } from "./data-parser";
 
 export async function getSheetsData() {
   try {
+    if (typeof window !== "undefined") {
+      throw new Error("NO SECRETS ON CLIENT!");
+    }
+    const { privateKey } = JSON.parse(
+      process.env.GOOGLE_PRIVATE_KEY || "{ privateKey : null }"
+    );
+
     const auth = await google.auth.getClient({
       scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
+      projectId: process.env.GOOGLE_PROJECT_ID,
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: privateKey,
+      },
     });
+
     const sheets = google.sheets({ version: "v4", auth });
     const range = `Form Responses 1`;
     const response = await sheets.spreadsheets.values.get({
