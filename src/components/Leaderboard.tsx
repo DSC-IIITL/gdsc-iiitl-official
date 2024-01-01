66b13b423babf528a941a62a30c793f7ec421022
savarnachandra20 <savarnachandra@gmail.com>
2024-01-02 00:21:57 +0530

    Updated styles in leaderboard - Made banner mobile friendly - Made leaderboard table responsive

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -12,13 +12,19 @@ import TableRow from "@mui/material/TableRow";
 import GithubLogo from "@/components/Logos/GithubLogo";
 import { LeaderboardData } from "@/lib/leaderboard";
 import { SeqBrandColor, capitalize } from "@/lib/utils";
+import { SxProps, Theme } from "@mui/material";
 
 export type LeaderboardProps = {
   data: LeaderboardData;
   pageSize?: number;
+  sx?: SxProps<Theme> | undefined;
 };
 
-export default function Leaderboard({ data, pageSize = 10 }: LeaderboardProps) {
+export default function Leaderboard({
+  data,
+  pageSize = 10,
+  sx,
+}: LeaderboardProps) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(pageSize);
 
@@ -44,11 +50,15 @@ export default function Leaderboard({ data, pageSize = 10 }: LeaderboardProps) {
   ] as (keyof (typeof data)[number])[];
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer
-        className="TableContainer"
-        sx={{ maxHeight: 440, scrollbarWidth: "1rem" }}
-      >
+    <Paper
+      sx={{
+        ...sx,
+        width: "100%",
+        display: "grid",
+        gridTemplateRows: "1fr auto",
+      }}
+    >
+      <TableContainer className="TableContainer">
         <Table stickyHeader aria-label="sticky table">
           <TableHead>
             <TableRow>
@@ -58,7 +68,7 @@ export default function Leaderboard({ data, pageSize = 10 }: LeaderboardProps) {
                   <TableCell
                     align="center"
                     className="HeadingColumn"
-                    sx={{ fontSize: "1.2vw", color: color }}
+                    sx={{ color }}
                     key={col}
                   >
                     {capitalize(col)}
@@ -77,14 +87,7 @@ export default function Leaderboard({ data, pageSize = 10 }: LeaderboardProps) {
                       switch (col) {
                         case "github":
                           return (
-                            <TableCell
-                              sx={{
-                                marginTop: "auto",
-                                marginBottom: "auto",
-                              }}
-                              align="center"
-                              key={col}
-                            >
+                            <TableCell align="center" key={col}>
                               {row[col] ? (
                                 <a
                                   href={
--- src/components/Logos/GDSCLogo.tsx
+++ src/components/Logos/GDSCLogo.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import GDSCLogoImage from "../../../public/gdsc.png";
 
-export default function GDSCLogo() {
+export default function GDSCLogo({
+  width = 128,
+  height = 128,
+}: {
+  width?: number;
+  height?: number;
+}) {
   return (
     <Image
       src={GDSCLogoImage}
       alt="GDSC Logo"
-      width={128}
-      height={128}
+      width={width}
+      height={height}
       style={{ borderRadius: "50%", userSelect: "none" }}
     />
   );
